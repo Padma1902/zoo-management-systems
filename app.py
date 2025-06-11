a582362b66e7d9c8a6d246154cd4ac9550cbd89a
Padma1902 <knpadmavathi846@gmail.com>
2025-06-11 21:43:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,35 +16,27 @@ def save_data(data):
         json.dump(data, f, indent=4)
 
 @app.route('/')
-def home():
-    return render_template("form.html")  # Only shows the form
+def index():
+    animals = load_data()
+    return render_template("index.html", animals=animals)
 
 @app.route('/add', methods=['POST'])
-def add_animal():
+def add():
+    name = request.form['name']
+    species = request.form['species']
+    age = request.form['age']
     data = load_data()
-    new_animal = {
-        "name": request.form['name'],
-        "species": request.form['species'],
-        "age": request.form['age'],
-        "caretaker": request.form['caretaker'],
-        "feeding_schedule": request.form['feeding_schedule'],
-        "vet_checkups": request.form['vet_checkups'],
-        "tickets": request.form['tickets'],
-        "visitors": request.form['visitors'],
-    }
-    data.append(new_animal)
+    data.append({"name": name, "species": species, "age": age})
     save_data(data)
-    return redirect("/records")  # Redirect to new page
-
-@app.route('/records')
-def show_records():
-    animals = load_data()
-    return render_template("records.html", animals=animals)
+    return redirect('/')
 
 @app.route('/delete/<int:index>')
-def delete_animal(index):
+def delete(index):
     data = load_data()
     if 0 <= index < len(data):
         data.pop(index)
         save_data(data)
-    return redirect("/records")
+    return redirect('/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
